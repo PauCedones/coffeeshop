7d44aa37edc863295a5e221a6381ce6e94b9f01c
la <la>
2020-01-27 12:59:22 -0300

    comienzo de la landing page

---
+++ src/secciones/01-header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Header.scss';
+
+const Header = () =>{
+    return(
+        <section>
+            <h1>Portwell, Inc.</h1>
+            <h2>Serving only the best since 2019</h2>
+           
+            
+        </section>
+    );
+}
+
+export default Header;
